d544e47f599d2e99f2f3c78ee7f06508d29a84b5
Professor MAD <professormad@proton.me>
2024-12-01 16:36:20 -0500

    land_card clean up

--- land_card.h
+++ land_card.h
@@ -5,45 +5,37 @@
 
 //Land Meta Data
 typedef struct {
-
     bool isMountain;
     bool isForest;
     bool isIsland;
     bool isPlains;
     bool isSwamp;
-
 } LandMetaData;
 
 //Land Mechanic Basics
 typedef struct {
-
     bool inHand;
     bool inPlay;
     bool inGraveyard;
     bool inLibrary;
     bool isTapped;
-
 } LandMechanicBasics;
 
 //Mana production
 typedef struct {
-
     bool producingMana;
     int RedManaAdded;
     int GreenManaAdded;
     int BlueManaAdded;
     int WhiteManaAdded;
     int BlackManaAdded;
-
 } ManaProduction;
 
 //Basic land struct
 typedef struct {
-
     LandMetaData landMetaData;
     LandMechanicBasics landMechanicBasics;
     ManaProduction manaProduction;
-
 } BasicLand;
 
 #endif
