3fe081e5ec960efe0f281b12d5d7cffaf4406446
Professor MAD <professormad@proton.me>
2025-01-11 16:49:39 -0500

    usage_home made and experiment fail

--- src/usage_home.c
+++ src/usage_home.c
@@ -6,11 +6,11 @@ void initializeUsageBox() {
     printf("|               ________                |\n");
     printf("|              |  HAND  |               |\n");
     printf("|              |________|               |\n");
-    printf("| ______      _____________      ______ |\n");
-    printf("|| CAST |    | BATTLEFIELD |    | GAME ||\n");
-    printf("||______|    |_____________|    |______||\n");
-    printf("|       __________          ____        |\n");
-    printf("|      | END TURN |        |QUIT|       |\n");
-    printf("|      |__________|        |____|       |\n");
+    printf("|  ______      _____________    ______  |\n");
+    printf("| | CAST |    | BATTLEFIELD |  | GAME | |\n");
+    printf("| |______|    |_____________|  |______| |\n");
+    printf("|       __________          ______      |\n");
+    printf("|      | END TURN |        | QUIT |     |\n");
+    printf("|      |__________|        |______|     |\n");
     printf(" --------------------------------------- \n");
 }
